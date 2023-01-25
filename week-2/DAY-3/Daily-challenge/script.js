31c2924dc06caaed14820409bda51bef36d10285
noa nakache <noa.nakache@icloud.com>
2023-01-25 09:22:33 +0200

    hey

---
+++ week-2/DAY-3/Daily-challenge/script.js
@@ -0,0 +1,16 @@
+
+for (let i = 0; i <= 0; i++) {
+    console.log("*");
+    for (let l = 0; l <= 0; l++) {
+      console.log("* *");
+    }
+    for (let n = 0; n <= 0; n++) {
+        console.log("* * *");
+      }
+      for (let y = 0; y <= 0; y++) {
+        console.log("* * * *");
+      }
+      for (let x = 0; x <= 0; x++) {
+        console.log("* * * * *");
+      }
+  }
