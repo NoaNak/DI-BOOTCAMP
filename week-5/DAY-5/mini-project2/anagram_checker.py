a85bb9e868e3562b7ff882c50fdf9e0e4f1dc0c0
noa nakache <noa.nakache@icloud.com>
2023-02-19 16:33:28 +0200

    hey

---
+++ week-5/DAY-5/mini-project2/anagram_checker.py
@@ -0,0 +1,8 @@
+class AnagramChecker:
+    def __init__(self):
+
+
+    def is_valid_word(word):
+
+    def get_anagrams(word):
+        
---
+++ week-6/DAY-1/SQL/SQL.sql
@@ -0,0 +1,22 @@
+-- create table items (
+-- id serial primary key, 
+-- items varchar(300), price int);
+-- -- id serial = faire automatiquement une liste de num 
+-- -- varchar = str
+-- -- preciser si c un str / int
+-- -- 300 cest la limite
+
+-- create table customers (
+-- id serial primary key,
+-- name varchar(100), lastname varchar(100));
+-- insert into items values
+-- (Default,'Small Desk',100),
+-- (Default,'Large desk',300),
+-- (Default,'Fan',80);
+-- chaque fois aque jecris default ca me met les chiffres automatiquement
+insert into customers values
+(Default,'Greg','Jones'),
+(Default,'Sandra','Jones'),
+(Default,'Scott','Scott'),
+(Default, 'Trevor','Green'),
+(Default, 'Melanie', 'Johnson');
